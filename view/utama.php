699e3adfeddbe0653f0e5b6c1cf874e0ea974e05
AriefLinux <79347655+Ariefna@users.noreply.github.com>
2022-05-01 14:56:45 +0700

    update

---
+++ view/utama.php
@@ -0,0 +1,11 @@
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+            <h1>ini isi</h1>
+        </div>
+    </div>
+    <!-- /.container-fluid -->
+</section>
+<!-- /.content -->
