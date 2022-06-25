abc10d28956cff027091f816e446d120e79b0fca
AriefLinux <79347655+Ariefna@users.noreply.github.com>
2022-06-25 17:34:35 +0700

    Update login.php

--- model/json/login.php
+++ model/json/login.php
@@ -14,22 +14,23 @@ if (isset($_POST['username'])) {
         $check = false;
     }
 
-    if ($ceklogin == 1 || $check) {
+    if ($ceklogin == 1 && $check) {
         $_SESSION['id_user'] = $user['id_user'];
         $_SESSION['nama'] = $user['nama_user'];
         $_SESSION['level'] = $user['level'];
         echo "ok";
     } else {
         $query = mysqli_query($koneksi, "select * from siswa where nisn='$username'");
-        $ceklogin = mysqli_num_rows($query);
-        $user = mysqli_fetch_array($query);
-        if ($ceklogin == 1 || password_verify($password, $user['password'])) {
-            $_SESSION['id_user'] = $user['id_siswa'];
-            $_SESSION['nama'] = $user['nama_siswa'];
+        $ceklogin1 = mysqli_num_rows($query);
+        $user1 = mysqli_fetch_array($query);
+        if ($ceklogin1 == 1 || password_verify($password, $user1['password'])) {
+            $_SESSION['id_user'] = $user1['id_siswa'];
+            $_SESSION['nama'] = $user1['nama_siswa'];
             $_SESSION['level'] = 'peserta';
             echo "ok";
         } else {
             echo "Password atau Username Anda salah";
+            // echo $ceklogin1 == 1 && password_verify($password, $user['password']);
         }
     }
 }
