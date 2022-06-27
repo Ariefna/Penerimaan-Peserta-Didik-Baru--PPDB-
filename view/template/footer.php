03eadf9a5681c1ea4e1f38d8ca81bc026c5936d6
AriefLinux <79347655+Ariefna@users.noreply.github.com>
2022-06-27 11:47:52 +0700

    Update footer.php

--- view/template/footer.php
+++ view/template/footer.php
@@ -1,8 +1,8 @@
 </div>
 <!-- /.content-wrapper -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021
-        <a href="http://localhost:8000/">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2022
+        <a href="http://localhost:8000/"><?php echo $setting['nama_sekolah']; ?></a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 3.2.0-rc
