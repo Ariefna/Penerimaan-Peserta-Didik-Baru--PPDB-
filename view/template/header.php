8e4b7a162cc6c509c00ddd597f91ea4d12211d9b
AriefLinux <79347655+Ariefna@users.noreply.github.com>
2022-07-21 23:22:25 +0700

    logo

--- view/template/header.php
+++ view/template/header.php
@@ -6,6 +6,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="icon" type="image/png" href="http://localhost:8000/<?= $setting['logo'] ?>" />
     <title><?php echo $setting['nama_sekolah']; ?></title>
 
     <!-- Font Awesome -->
